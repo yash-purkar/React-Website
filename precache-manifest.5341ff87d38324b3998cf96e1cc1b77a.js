6b933e6dd80afb42cc1359bcaa54249072aeb908
yashpurkar <yashpurkar7079@gmail.com>
2022-10-01 01:05:07 +0530

    Updates

---
+++ precache-manifest.5341ff87d38324b3998cf96e1cc1b77a.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "9c6975dd91821db232f5",
+    "url": "/React-website/static/css/main.ac0e9878.chunk.css"
+  },
+  {
+    "revision": "9c6975dd91821db232f5",
+    "url": "/React-website/static/js/main.9c6975dd.chunk.js"
+  },
+  {
+    "revision": "598c8e149a4058703e66",
+    "url": "/React-website/static/css/1.a2a7d99e.chunk.css"
+  },
+  {
+    "revision": "598c8e149a4058703e66",
+    "url": "/React-website/static/js/1.598c8e14.chunk.js"
+  },
+  {
+    "revision": "f6b72dd7eca95d626f29",
+    "url": "/React-website/static/js/runtime~main.f6b72dd7.js"
+  },
+  {
+    "revision": "d1f539ce6cfd9c494f1b1653defff8f8",
+    "url": "/React-website/static/media/img-4.d1f539ce.svg"
+  },
+  {
+    "revision": "6011b68b274fe15c42b75a493c60cab7",
+    "url": "/React-website/static/media/img-3.6011b68b.svg"
+  },
+  {
+    "revision": "b4f796245e7b8849ddd8a90541a2c45e",
+    "url": "/React-website/static/media/Web_Dev.b4f79624.jpg"
+  },
+  {
+    "revision": "1a752ff26a51c3e602627e1faee629dd",
+    "url": "/React-website/static/media/App_Dev.1a752ff2.jpg"
+  },
+  {
+    "revision": "d4ed114d6959183d55c4ac260fcd5497",
+    "url": "/React-website/static/media/Software_Dev.d4ed114d.jpg"
+  },
+  {
+    "revision": "78fec3d6d2c81294346fa618ba0caf00",
+    "url": "/React-website/static/media/Digi_Mark.78fec3d6.jpg"
+  },
+  {
+    "revision": "ade849958754273deb8581503d76b88e",
+    "url": "/React-website/static/media/Android_Dev.ade84995.jpg"
+  },
+  {
+    "revision": "5292798602edc3452bc8cb106f8c4109",
+    "url": "/React-website/static/media/Marketing.52927986.jpg"
+  },
+  {
+    "revision": "95d26e7da8a3d0b0f855c2fc1126e943",
+    "url": "/React-website/index.html"
+  }
+];
